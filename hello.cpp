0229746983591381b2e87bbdb8690bd9c4816208
wind5293 <mywind5293@gmail.com>
2024-10-11 21:55:18 +0700

    2nd test

---
+++ hello.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#define N 10007
+using namespace std;
+
+string isBalanced(string s) {
+    int stack[N] = {};
+    int i = 0;
+    
+    for (char ch : s) {
+        if (ch == '{') {
+            stack[i] = 3;
+            i++;
+        }
+        if (ch == '[') {
+            stack[i] = 2;
+            i++;
+        }
+        if (ch == '(') {
+            stack[i] = 1;
+            i++;
+        }
+        if (ch == ')') {
+            if (i > 0 && stack[i - 1] == 1) {
+                i--;
+            } else {
+                return "NO";
+            }
+        }
+        if (ch == ']') {
+            if (i > 0 && stack[i - 1] == 2) {
+                i--;
+            } else {
+                return "NO";
+            }
+        }
+        if (ch == '}') {
+            if (i > 0 && stack[i - 1] == 3) {
+                i--;
+            } else {
+                return "NO";
+            }
+        }
+    }
+    return (i == 0 ? "YES" : "NO");
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int t; cin >> t;
+    while (t--) {
+        string s; cin >> s;
+        cout << isBalanced(s) << endl;
+    }
+}
